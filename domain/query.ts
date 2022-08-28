3c360fc35d04aeef65e5d661897d11e51c6867b2
krastsislau <krastsislau@gmail.com>
2022-08-29 00:10:08 +0300

    infinitely scrollable feed demo completed

--- domain/query.ts
+++ domain/query.ts
@@ -1,7 +1,67 @@
-import client from "../apollo-client";
 import { gql } from "@apollo/client";
+import client from "../apollo-client";
+import { Feed } from "./types";
 
+// Just get all links with all the info
+const getAllLinks = async () : Promise<Feed> => {
+    return client.query({
+        query: gql`
+            query {
+              feed {
+                count
+                links {
+                  id
+                  description
+                  url
+                  postedBy {
+                    id
+                    name
+                  }
+                  votes {
+                    id
+                    user {
+                      id
+                      name
+                    }
+                  }
+                }
+              }
+            }
+        `,
+    }).then(res => res.data);
+}
 
+// get filtered, ordered, and paginated links
+const getFilteredOrderedPaginatedLinks =
+    async (pattern: string,
+           // todo: add order rules
+           take: number,
+           skip: number) : Promise<Feed> => {
+    return client.query({
+        query: gql`
+            query {
+              feed(take: ${take}, skip: ${skip}, filter: "${pattern}") {
+                count
+                links {
+                  id
+                  description
+                  url
+                  postedBy {
+                    id
+                    name
+                  }
+                  votes {
+                    id
+                    user {
+                      id
+                      name
+                    }
+                  }
+                }
+              }
+            }
+        `,
+    }).then(res => res.data.feed);
+}
 
-
-export {};
+export { getAllLinks, getFilteredOrderedPaginatedLinks };
--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,1 +1,52 @@
-export {};
+import { useState } from "react";
+import type { NextPage } from 'next';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { getAllLinks, getFilteredOrderedPaginatedLinks } from "../../domain/query";
+import { signIn, signUp } from "../../domain/mutation";
+import { Feed, Link } from "../../domain/types";
+
+export async function getServerSideProps() {
+    return getFilteredOrderedPaginatedLinks("", 10, 0)
+        .then(data => {
+            return { props: { ...data } }
+        });
+}
+
+const Feed: NextPage = (props: any) => {
+    const [feed, setFeed] = useState<Feed>(props);
+    const [hasMore, setHasMore] = useState<boolean>(true);
+
+    const getMoreLinks = async () => {
+        const moreFeed = await getFilteredOrderedPaginatedLinks(
+            "", 10, feed.links.length
+        );
+        console.log(moreFeed);
+        if (moreFeed.links.length === 0) {
+            setHasMore(false);
+        } else {
+            setFeed(prev => ({
+                count: prev.count,
+                links: [...prev.links, ...moreFeed.links],
+            }));
+        }
+    };
+
+    return (
+        <InfiniteScroll
+            dataLength={feed.links.length}
+            next={getMoreLinks}
+            hasMore={hasMore}
+            loader={<h3>Loading...</h3>}
+            endMessage={<h3>You've reached the bottom of the feed</h3>}>
+                { feed.links.map((link: Link) => <div style={{
+                    border: "red solid 1px",
+                    height: 100
+                }}>
+                    { link.description }
+                </div>) }
+        </InfiniteScroll>
+    );
+};
+
+export default Feed;
